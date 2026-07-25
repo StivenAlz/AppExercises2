cab086107a2e24641bcedd5612d1204abb28b911
StivenAlz <stivenalomaliza@hotmail.com>
2026-07-25 14:48:07 -0500

    MOD: Chistes actualizados

--- Bienvenute/obj/Release/net10.0/linux-x64/Bienvenute.AssemblyInfo.cs
+++ Bienvenute/obj/Release/net10.0/linux-x64/Bienvenute.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bienvenute")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d52a7c3a138252911b7c5441ec9a60058ac58870")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bienvenute")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bienvenute")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
